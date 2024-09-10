7e58810e0148fc1f00cbf43d36b966abb7d945ac
Murilo Figueiredo <murilocarvalho.f@gmail.com>
2024-09-10 10:43:57 -0300

    inclusão dos codigos conforme dessafio proposto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        // Conhecer e importar a classe Scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US) ;
+            // Exibir as mensagens para o nosso usuario
+            System.out.println("bem vindo ao seu banco digital");
+
+            // Obter pela scanner os valores digitados no terminal
+            System.out.println("Digite seu nome completo");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu CPF");
+            int cpf = scanner.nextInt();
+
+            System.out.println("Digite seu RG");
+            int rg = scanner.nextInt();
+   
+            
+            // Exibir a mensagem conta criada
+            System.out.println("Conta criada para:" + nome);
+            System.out.println("CPF:"+ cpf);
+            System.out.println("RG:" + rg );
+        }
+
+        
+
+
+        
     }
-}
+
+
